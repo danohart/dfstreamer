a704ef8b1676ffb5e99a997c540969b632b01a82
Daniel Hart <me@danielhart.co>
2020-04-22 21:24:19 -0500

    Update welcome image

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div className="header">
       <div className="welcome center-align">
-        <img src="/welcome.png" />
+        <img src="/static/welcome.png" />
       </div>
       <h2>Live Music from Safe Spaces</h2>
     </div>
