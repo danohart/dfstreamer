f0a0b547d8b40cb85ede38d03b2fee1772e96439
Daniel Hart <me@danielhart.co>
2020-04-28 20:54:39 -0500

    Fix up some tablet styling for pop out chat

--- frontend/components/Events.js
+++ frontend/components/Events.js
@@ -3,7 +3,7 @@ import EventHost from './EventHost';
 function Events() {
   return (
     <>
-      {/* <h2 className="center-align">Events</h2> */}
+      <h2 className="center-align">Schedule</h2>
       <div className="events-container">
         <EventHost stage="df_thelivingroom" />
         <EventHost stage="df_thebedroom" />
