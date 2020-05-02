982da3d80269f4c1a4ecb7041a74b0f3aeb85b92
Daniel Hart <me@danielhart.co>
2020-05-02 00:57:12 -0500

    Add date

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -5,6 +5,9 @@ function Header() {
         <h1>Welcome To Distance Fest</h1>
       </div>
       <h2>Live Music from Safe Spaces</h2>
+      <h3 className="center-align" data-text="May 16-17">
+        May 16-17
+      </h3>
     </div>
   );
 }
