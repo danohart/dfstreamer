523821a1be5a973ad06d194cd66790e703ba0df1
Daniel Hart <me@danielhart.co>
2020-05-17 22:52:02 -0500

    Add artist sub form

--- frontend/pages/artists.js
+++ frontend/pages/artists.js
@@ -1,15 +1,6 @@
 import React from 'react';
+import ArtistSubmission from '../components/ArtistSubmission';
 
 export default function Artists() {
-  return (
-    <iframe
-      height='1000'
-      title='Embedded Form'
-      allowtransparency='true'
-      frameBorder='0'
-      scrolling='no'
-      style={{ width: '100%', border: 'none', color: 'white' }}
-      src='https://danohart.wufoo.com/embed/qxlil4n1bcmypk/'
-    ></iframe>
-  );
+  return <ArtistSubmission />;
 }
